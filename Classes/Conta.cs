85ac287f673df1a6e295f77a65a464bc91496715
pedroabreumaia <pedroam956@gmail.com>
2021-04-24 20:14:25 -0300

    Adiciona a classe Agencia e o atributo Senha a classe Conta e métodos para funcionalidades de agências na classe Agencia e Program

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -6,17 +6,28 @@ namespace DIO.Bank
 	{
 		// Atributos
 		private TipoConta TipoConta { get; set; }
+		private long Agencia { get; set; }
+		private long NumConta { get; set; }
 		private double Saldo { get; set; }
 		private double Credito { get; set; }
 		private string Nome { get; set; }
+		private string Senha { get; set; }
 
 		// Métodos
-		public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+		public Conta(TipoConta tipoConta, long agencia, long numConta, double saldo, double credito, string nome, string senha)
 		{
 			this.TipoConta = tipoConta;
+			this.Agencia = agencia;
+			this.NumConta = numConta;
 			this.Saldo = saldo;
 			this.Credito = credito;
 			this.Nome = nome;
+			this.Senha = senha;
+		}
+
+		public bool ValidaSenhas(string senha)
+		{
+			return this.Senha.Equals(senha);
 		}
 
 		public bool Sacar(double valorSaque)
@@ -41,18 +52,14 @@ namespace DIO.Bank
             Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
 		}
 
-		public void Transferir(double valorTransferencia, Conta contaDestino)
-		{
-			if (this.Sacar(valorTransferencia)){
-                contaDestino.Depositar(valorTransferencia);
-            }
-		}
-
         public override string ToString()
 		{
             string retorno = "";
             retorno += "TipoConta " + this.TipoConta + " | ";
+			retorno += "Agência " + this.Agencia + " | ";
+			retorno += "Nº Conta " + this.NumConta + " | ";
             retorno += "Nome " + this.Nome + " | ";
+			retorno += "Senha " + this.Senha + " | ";
             retorno += "Saldo " + this.Saldo + " | ";
             retorno += "Crédito " + this.Credito;
 			return retorno;
--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography; 
 
 namespace DIO.Bank
 {
 	class Program
 	{
-		static List<Conta> listContas = new List<Conta>();
+		static List<Agencia> listAgencia = new List<Agencia>();
+		
 		static void Main(string[] args)
 		{
+			listAgencia.Add(new Agencia(0));
+			listAgencia.Add(new Agencia(1));
+			listAgencia.Add(new Agencia(2));
+			listAgencia.Add(new Agencia(3));
+
 			string opcaoUsuario = ObterOpcaoUsuario();
 
 			while (opcaoUsuario.ToUpper() != "X")
@@ -15,18 +23,21 @@ namespace DIO.Bank
 				switch (opcaoUsuario)
 				{
 					case "1":
-						ListarContas();
+						ListarAgencias();
 						break;
 					case "2":
-						InserirConta();
+						ListarContas();
 						break;
 					case "3":
-						Transferir();
+						InserirConta();
 						break;
 					case "4":
-						Sacar();
+						Transferir();
 						break;
 					case "5":
+						Sacar();
+						break;
+					case "6":
 						Depositar();
 						break;
                     case "C":
@@ -46,43 +57,75 @@ namespace DIO.Bank
 
 		private static void Depositar()
 		{
+			Console.Write("Digite o número da agência: ");
+			int indiceAgencia = int.Parse(Console.ReadLine());
+
 			Console.Write("Digite o número da conta: ");
 			int indiceConta = int.Parse(Console.ReadLine());
 
 			Console.Write("Digite o valor a ser depositado: ");
 			double valorDeposito = double.Parse(Console.ReadLine());
 
-            listContas[indiceConta].Depositar(valorDeposito);
+            listAgencia[indiceAgencia].Depositar(indiceConta, valorDeposito);
 		}
 
 		private static void Sacar()
 		{
+			Console.Write("Digite o número da agência: ");
+			int indiceAgencia = int.Parse(Console.ReadLine());
+
 			Console.Write("Digite o número da conta: ");
 			int indiceConta = int.Parse(Console.ReadLine());
 
-			Console.Write("Digite o valor a ser sacado: ");
-			double valorSaque = double.Parse(Console.ReadLine());
+			Console.Write("Digite a senha: ");
+			string entradaSenha = Console.ReadLine(); 
 
-            listContas[indiceConta].Sacar(valorSaque);
+			string senhaSHA256 = ComputeSha256Hash(entradaSenha);
+			if (listAgencia[indiceAgencia].ValidarSenha(indiceConta, senhaSHA256)) {
+				Console.Write("Digite o valor a ser sacado: ");
+				double valorSaque = double.Parse(Console.ReadLine());
+
+				listAgencia[indiceAgencia].Sacar(indiceConta, valorSaque);
+			} else {
+				Console.Write("Senha inválida! Operação cancelada.");
+			}
 		}
 
 		private static void Transferir()
 		{
+			Console.Write("Digite o número da agência de origem: ");
+			int indiceAgenciaOrigem = int.Parse(Console.ReadLine());
+
 			Console.Write("Digite o número da conta de origem: ");
 			int indiceContaOrigem = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o número da conta de destino: ");
-			int indiceContaDestino = int.Parse(Console.ReadLine());
+			Console.Write("Digite a senha: ");
+			string entradaSenha = Console.ReadLine(); 
 
-			Console.Write("Digite o valor a ser transferido: ");
-			double valorTransferencia = double.Parse(Console.ReadLine());
+			string senhaSHA256 = ComputeSha256Hash(entradaSenha);
+			if (listAgencia[indiceAgenciaOrigem].ValidarSenha(indiceContaOrigem, senhaSHA256)) {
 
-            listContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+				Console.Write("Digite o número da agência de destino: ");
+				int indiceAgenciaDestino = int.Parse(Console.ReadLine());
+
+				Console.Write("Digite o número da conta de destino: ");
+				int indiceContaDestino = int.Parse(Console.ReadLine());
+
+				Console.Write("Digite o valor a ser transferido: ");
+				double valorTransferencia = double.Parse(Console.ReadLine());
+
+				listAgencia[indiceAgenciaOrigem].Transferir(indiceContaOrigem, listAgencia[indiceAgenciaDestino], indiceContaDestino, valorTransferencia);
+			} else {
+				Console.Write("Senha inválida! Operação cancelada.");
+			}
 		}
 
 		private static void InserirConta()
 		{
 			Console.WriteLine("Inserir nova conta");
+
+			Console.Write("Digite o número da agência: ");
+			int indiceAgencia = int.Parse(Console.ReadLine());
 
 			Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
 			int entradaTipoConta = int.Parse(Console.ReadLine());
@@ -90,35 +133,46 @@ namespace DIO.Bank
 			Console.Write("Digite o Nome do Cliente: ");
 			string entradaNome = Console.ReadLine();
 
+			Console.Write("Digite a senha: ");
+			string entradaSenha = Console.ReadLine();
+
 			Console.Write("Digite o saldo inicial: ");
 			double entradaSaldo = double.Parse(Console.ReadLine());
 
 			Console.Write("Digite o crédito: ");
 			double entradaCredito = double.Parse(Console.ReadLine());
 
-			Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
-										saldo: entradaSaldo,
-										credito: entradaCredito,
-										nome: entradaNome);
+			string senhaSHA256 = ComputeSha256Hash(entradaSenha);
 
-			listContas.Add(novaConta);
+			listAgencia[indiceAgencia].InserirConta(entradaTipoConta: entradaTipoConta, entradaNome: entradaNome, 
+														entradaSenha: senhaSHA256, entradaSaldo: entradaSaldo, entradaCredito: entradaCredito);
 		}
 
 		private static void ListarContas()
 		{
 			Console.WriteLine("Listar contas");
 
-			if (listContas.Count == 0)
+			Console.Write("Digite o número da agência: ");
+			int indiceAgencia = int.Parse(Console.ReadLine());
+
+			listAgencia[indiceAgencia].ListarContas();			
+		}
+
+		private static void ListarAgencias() 
+		{
+			Console.WriteLine("Listar Agências");
+
+			if (listAgencia.Count == 0)
 			{
-				Console.WriteLine("Nenhuma conta cadastrada.");
+				Console.WriteLine("Nenhuma agência cadastrada.");
 				return;
 			}
 
-			for (int i = 0; i < listContas.Count; i++)
+			for (int i = 0; i < listAgencia.Count; i++)
 			{
-				Conta conta = listContas[i];
+				Agencia agencia = listAgencia[i];
 				Console.Write("#{0} - ", i);
-				Console.WriteLine(conta);
+				Console.WriteLine(agencia);
 			}
 		}
 
@@ -128,11 +182,12 @@ namespace DIO.Bank
 			Console.WriteLine("DIO Bank a seu dispor!!!");
 			Console.WriteLine("Informe a opção desejada:");
 
-			Console.WriteLine("1- Listar contas");
-			Console.WriteLine("2- Inserir nova conta");
-			Console.WriteLine("3- Transferir");
-			Console.WriteLine("4- Sacar");
-			Console.WriteLine("5- Depositar");
+			Console.WriteLine("1- Listar agências");
+			Console.WriteLine("2- Listar contas");
+			Console.WriteLine("3- Inserir nova conta");
+			Console.WriteLine("4- Transferir");
+			Console.WriteLine("5- Sacar");
+			Console.WriteLine("6- Depositar");
             Console.WriteLine("C- Limpar Tela");
 			Console.WriteLine("X- Sair");
 			Console.WriteLine();
@@ -141,5 +196,23 @@ namespace DIO.Bank
 			Console.WriteLine();
 			return opcaoUsuario;
 		}
+
+		private static string ComputeSha256Hash(string rawData)  
+        {  
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));  
+  
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();  
+                for (int i = 0; i < bytes.Length; i++)  
+                {  
+                    builder.Append(bytes[i].ToString("x2"));  
+                }  
+                return builder.ToString();  
+            }  
+        }  
 	}
 }
